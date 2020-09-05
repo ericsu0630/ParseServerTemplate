85e4819649e7894a94b70221c0dbbf89c53aa428
Eric Su <ericsu0630@gmail.com>
2020-09-05 17:17:56 +0800

    Use this template for future Parse server projects

---
+++ app/src/main/java/com/example/parseservertemplate/StarterApplication.java
@@ -0,0 +1,34 @@
+package com.example.parseservertemplate;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class StarterApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        //http://54.238.88.134/apps
+        //Username, password: user, 0Stl3l8eU0Lu
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                //update this information for new server instance
+                .applicationId("myappID")
+                .clientKey("0Stl3l8eU0Lu")
+                .server("http://54.238.88.134/parse/")
+                .build()
+        );
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
